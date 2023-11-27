79100bd8835265a1ce7663601a48fc9e1c1fdb80
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-11-27 20:25:48 +0200

    implementing needed guards

---
+++ src/guards/manager-auth.guard.ts
@@ -0,0 +1,44 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class ManagerAuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.configService.getOrThrow('JWT_SECRET'),
+      });
+      if (payload.role !== 'manager') {
+        throw new UnauthorizedException('Unauthorized access not manager');
+      }
+      // 💡 I'm assigning the payload to the request object here
+      // so that I can access it in our route handlers
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException('Unauthorized access');
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
