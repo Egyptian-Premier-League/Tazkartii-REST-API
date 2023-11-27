3c97872ffc4d040eb771adf3f8c35999161d3221
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-11-27 20:26:38 +0200

    configure database and config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath:
+        process.env.NODE_ENV === 'development' ? `.env.development` : '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        if (process.env.NODE_ENV === 'development') {
+          return {
+            type: 'postgres',
+            ssl: process.env.NODE_ENV === 'development' ? false : true,
+            host: config.getOrThrow<string>('DATABASE_HOST'),
+            port: config.getOrThrow<number>('DATABASE_PORT'),
+            database: config.getOrThrow<string>('DATABASE_NAME'),
+            username: config.getOrThrow<string>('DATABASE_USER'),
+            password: config.getOrThrow<string>('DATABASE_PASSWORD'),
+            entities: [],
+            logger: 'file',
+            logging: false,
+            synchronize: true,
+          };
+        }
+      },
+    }),
+    JwtModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.getOrThrow('JWT_SECRET'),
+        signOptions: { expiresIn: '7d' },
+        global: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
