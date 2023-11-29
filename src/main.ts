0dba708522ec4b4597901446ef675cd117507dbf
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-11-29 15:51:42 +0200

    Update default port

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT || 300);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
