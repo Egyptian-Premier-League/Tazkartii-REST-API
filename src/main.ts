29f390082c2bce0c7984ba7a7140d9d751cb8f78
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-11-29 15:47:36 +0200

    assign the port dynamically

--- src/main.ts
+++ src/main.ts
@@ -54,6 +54,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 300);
 }
 bootstrap();
