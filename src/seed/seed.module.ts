1272e82e57530862862041e887f6cdfbf59baf4b
Zeyad Tarek <zeyad.ta01@gmail.com>
2023-12-13 00:19:57 +0200

    Seed needed teams

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdminsModule } from 'src/admins/admins.module';
 import { Admin } from 'src/admins/entities/admin.entity';
 import { AdminSeed } from './admin.seed';
+import { Team } from 'src/general/entities/team.entity';
+import { TeamSeed } from './team.seed';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Admin]), AdminsModule],
-  providers: [AdminSeed],
-  exports: [AdminSeed],
+  imports: [TypeOrmModule.forFeature([Admin, Team]), AdminsModule],
+  providers: [AdminSeed, TeamSeed],
+  exports: [AdminSeed, TeamSeed],
 })
 export class SeedModule {}
---
+++ src/seed/team.seed.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Admin } from 'src/admins/entities/admin.entity';
+import { AdminsService } from 'src/admins/admins.service';
+import { CreateAdminDto } from 'src/admins/dtos/create-admin.dto';
+import { ConfigService } from '@nestjs/config';
+import { Team } from 'src/general/entities/team.entity';
+import * as teamsDataJson from './teams.json';
+
+@Injectable()
+export class TeamSeed {
+  constructor(
+    @InjectRepository(Team)
+    private readonly teamRepository: Repository<Team>,
+    private configService: ConfigService,
+  ) {}
+
+  async seedTeams() {
+    const teamExist = (await this.teamRepository.count()) > 0;
+    if (!teamExist) {
+      const teamsData = teamsDataJson;
+      const teams = [];
+      for (const team of teamsData) {
+        const teamDb = this.teamRepository.create({
+          name: team.name,
+          photoUrl: team.photo,
+        });
+        teams.push(teamDb);
+      }
+      await this.teamRepository.save(teams);
+    }
+  }
+}
